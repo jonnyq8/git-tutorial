8461c33872a00798d9ec8f37605e0e7e8a8b0eea
Jonathan Forrester <jonnyq8@gmail.com>
2025-02-13 23:25:41 -0500

    added jon

---
+++ src/Jon.java
@@ -0,0 +1,14 @@
+public class Jon {
+    
+        String fname="Jonathan";
+        int age=25;
+        Jon(){
+            this.fname="jonathan";
+            this.age=25;
+        }
+        public static void main(String[] args) {
+            Jon forrester=new Jon();
+            System.out.println("Name: "+forrester.fname+" Age: "+forrester.age);
+        }
+    
+}
